fd2250e312b8c91ebf81c736761b32a272c60d40
Xene-Sky <43412070+Xene-Sky@users.noreply.github.com>
2018-09-27 22:23:30 +0200

    Update Matiere.php

--- Matiere.php
+++ Matiere.php
@@ -3,14 +3,16 @@
 		private $nomMatiere;
 		private $desciption;
 		private $date;
+		private $intervenant
 
-		public function __construct($myNom, $myDesc, $myDate) {
+		public function __construct($myNom, $myDesc, $myDate, $myinter) {
 			$this->nomMatiere = $myNom;
 			$this->description = $myDesc;
 			$this->date = $myDate;
+			$this->intervenant = $myinter
 		}
 		public function getAllInfo() {
-			$tabInfo = [$this->nomMatiere, $this->description, $this->date];
+			$tabInfo = [$this->nomMatiere, $this->description, $this->date, $this->intervenant];
 			return $tabInfo;
 			echo '$tabInfo';
 		}
