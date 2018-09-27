85be38a2faf69232ae412608fd0219af68a358d4
YanisVerbeke <43301002+YanisVerbeke@users.noreply.github.com>
2018-09-27 14:21:58 +0200

    Add files via upload

---
+++ Intervenant.php
@@ -0,0 +1,32 @@
+<?php 
+	class Intervenant {
+		private $nom;
+		private $prenom;
+		private $mail;
+		private $tel;
+		private $site;
+
+		public function __construct($myNom, $myPrenom, $myMail, $myTel, $mySite) {
+			$this->nom = $myNom;
+			$this->prenom = $myPrenom;
+			$this->mail = $myMail;
+			$this->tel = $myTel;
+			$this->site = $mySite;
+		}
+		public function getNom() {
+		return $this->nom;
+		}
+		public function getPrenom() {
+		return $this->prenom;
+		}
+		public function getMail() {
+		return $this->mail;
+		}
+		public function getTel() {
+		return $this->tel;
+		}
+		public function getSite() { 
+		return $this->site;
+	}
+	}
+?>
